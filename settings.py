cd628b4c751f7f2a86126548cbd0f68e0cd3a616
Omar Alqahtani <o.t.r.alqahtani@student.reading.ac.uk>
2021-07-17 11:33:03 +0100

    'm'

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 TRACK_WORDS = ['depression', 'games']
 TABLE_NAME = "Mental_Health"
-TABLE_ATTRIBUTES = "id_str VARCHAR(255), created_at DATETIME, text VARCHAR(255), \
+TABLE_ATTRIBUTES = "id_str VARCHAR(255), created_at timestamp, text VARCHAR(255), \
             depression VARCHAR(255), user_created_at VARCHAR(255), user_location VARCHAR(255), \
-            user_description VARCHAR(255), user_followers_count INT, longitude DOUBLE, latitude DOUBLE, \
+            user_description VARCHAR(255), user_followers_count INT, longitude DOUBLE PRECISION, latitude DOUBLE PRECISION, \
             retweet_count INT, favorite_count INT"
