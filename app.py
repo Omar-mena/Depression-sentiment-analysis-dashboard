35964226633984774c785f1040c1b482697af48f
Omar Alqahtani <o.t.r.alqahtani@student.reading.ac.uk>
2021-07-19 11:55:55 +0100

    'm'

--- app.py
+++ app.py
@@ -128,14 +128,14 @@ def update_graph_live(n):
     df = pd.read_sql(query, con=conn)
 
     # Convert UTC into BST
-   # df['created_at'] = pd.to_datetime(df['created_at']).apply(lambda x: x - datetime.timedelta(hours=1))
+    df['created_at'] = pd.to_datetime(df['created_at']).apply(lambda x: x - datetime.timedelta(hours=1))
 
     # Clean and transform data to enable time series
     result = df.groupby([pd.Grouper(key='created_at', freq='10s'), 'depression']).count().unstack(
         fill_value=0).stack().reset_index()
     result = result.rename(
-        columns={"id_str": "Num of '{}' mentions".format(settings.TRACK_WORDS[0]), "created_at": "Time in BTS"})
-    time_series = result["Time in BTS"][result['depression'] == 'positive' or 'negative'].reset_index(drop=True)
+        columns={"id_str": "Num of '{}' mentions".format(settings.TRACK_WORDS[0]), "created_at": "Time"})
+    time_series = result["Time"][result['depression'] == 'positive'].reset_index(drop=True)
 
     min10 = datetime.datetime.now() - datetime.timedelta(hours=1, minutes=10)
     min20 = datetime.datetime.now() - datetime.timedelta(hours=1, minutes=20)
