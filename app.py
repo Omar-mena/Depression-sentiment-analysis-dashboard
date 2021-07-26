353b7fa8b0bcb455db96b92c16d59070f68eb7c2
Omar Alqahtani <o.t.r.alqahtani@student.reading.ac.uk>
2021-07-26 11:25:03 +0100

    'm'

--- app.py
+++ app.py
@@ -8,32 +8,21 @@ import plotly.graph_objs as go
 import settings
 import itertools
 import math
+import base64
+from flask import Flask
 import os
 import psycopg2
 import datetime
 import re
 import nltk
+
 nltk.download('punkt')
 nltk.download('stopwords')
 from nltk.probability import FreqDist
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-nltk.download('stopwords')
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-from tqdm import tqdm
-from nltk.corpus import stopwords
-stopwordEn = stopwords.words('english')
-from nltk.corpus import wordnet
-nltk.download('wordnet')
-import re, string, unicodedata
-import nltk
-from nltk import word_tokenize, sent_tokenize, pos_tag
-from nltk.corpus import stopwords
-from nltk.stem import LancasterStemmer, WordNetLemmatizer
-from nltk.stem.porter import PorterStemmer
+from textblob import TextBlob
 from joblib import load
-import pickle
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -299,101 +288,6 @@ def update_graph_live(n):
     ]
     return children
 
-def tokenization(word):
-  word = nltk.word_tokenize(word)
-  return word
-
-
-def lemmatize_sentence_And_POStag(tokens):
-    lemmatizer = WordNetLemmatizer()
-    porter = PorterStemmer()
-    #stem_tokens = porter.stem(tokens)
-    lemmatized_sentence = []
-    for word, tag in pos_tag(tokens):
-        if tag.startswith('NN'):
-            pos = 'n'
-        elif tag.startswith('VB'):
-            pos = 'v'
-        elif tag.startswith('ADJ'):
-            pos = 'j'
-        else:
-            pos = 'a'
-        lemmatized_sentence.append(lemmatizer.lemmatize(word, pos))
-    return lemmatized_sentence
-
-
-def remove_non_ascii(words):
-    """Remove non-ASCII characters from list of tokenized words"""
-    new_words = []
-    for word in words:
-        new_word = unicodedata.normalize('NFKD', word).encode('ascii', 'ignore').decode('utf-8', 'ignore')
-        new_words.append(new_word)
-    return new_words
-
-def to_lowercase(words):
-    """Convert all characters to lowercase from list of tokenized words"""
-    new_words = []
-    for word in words:
-        new_word = word.lower()
-        new_words.append(new_word)
-    return new_words
-
-def remove_punctuation(words):
-    """Remove punctuation from list of tokenized words"""
-    new_words = []
-    for word in words:
-        new_word1 = re.sub(r'[^\w\s]', '', word)
-        new_word = re.sub(r'(https|http)?:\/\/(\w|\.|\/|\?|\=|\&|\%)*\b|@\w+|#', '', new_word1, flags=re.MULTILINE)
-        if new_word != '':
-            new_words.append(new_word)
-    return new_words
-
-def replace_numbers(words):
-    """Replace all interger occurrences in list of tokenized words with textual representation"""
-    p = inflect.engine()
-    new_words = []
-    for word in words:
-        if word.isdigit():
-            new_word = p.number_to_words(word)
-            new_words.append(new_word)
-        else:
-            new_words.append(word)
-    return new_words
-
-def remove_stopwords(words):
-    """Remove stop words from list of tokenized words"""
-    new_words = []
-    for word in words:
-        if word not in stopwords.words('english'):
-            new_words.append(word)
-    return new_words
-
-def stem_words(words):
-    """Stem words in list of tokenized words"""
-    stemmer = LancasterStemmer()
-    stems = []
-    for word in words:
-        stem = stemmer.stem(word)
-        stems.append(stem)
-    return stems
-
-def lemmatize_verbs(words):
-    """Lemmatize verbs in list of tokenized words"""
-    lemmatizer = WordNetLemmatizer()
-    lemmas = []
-    for word in words:
-        lemma = lemmatizer.lemmatize(word, pos='v')
-        lemmas.append(lemma)
-    return lemmas
-
-def normalize(words):
-    words = tokenization(words)
-    words = remove_non_ascii(words)
-    words = to_lowercase(words)
-    words = remove_punctuation(words)
-    words = replace_numbers(words)
-    words = remove_stopwords(words)
-    return words
 
 @app.callback(Output('live-update-graph-bottom', 'children'),
               [Input('interval-component-slow', 'n_intervals')])
@@ -455,22 +349,14 @@ def update_graph_bottom_live(n):
         if (w not in stop_words) and (len(w) >= 3):
             filtered_sent.append(w)
     fdist = FreqDist(filtered_sent)
-
-
-
-
-
-    clf = load('depression_model1.joblib')
-
     fd = pd.DataFrame(fdist.most_common(16), columns=["Word", "Frequency"]).drop([0]).reindex()
-    #fd['depression'] = fd['Word'].apply(lambda x: clf.predict(pd.DataFrame(StringIO(x), columns=["text"]).text))
-    fd['depression'] = clf.predict(fd['Word'])
+    fd['depression'] = fd['Word'].apply(lambda x: TextBlob(x).sentiment.polarity)
     fd['Marker_Color'] = fd['depression'].apply(lambda x: 'rgba(255, 50, 50, 0.6)' if x < -0.1 else \
         ('rgba(184, 247, 212, 0.6)' if x > 0.1 else 'rgba(131, 90, 241, 0.6)'))
     fd['Line_Color'] = fd['depression'].apply(lambda x: 'rgba(255, 50, 50, 1)' if x < -0.1 else \
         ('rgba(184, 247, 212, 1)' if x > 0.1 else 'rgba(131, 90, 241, 1)'))
-    print(fd['depression'])
-    print(fd)
+
+
 
     # Create the graph
     children = [
